96a4bab80fcbcf94dbc5cb5ae316e57ab11ad5b8
reysonc0sta <reysoncosta8@gmail.com>
2026-09-08 16:47:55 -0400

    teste - excluir

--- backend/app/services/config_service.py
+++ backend/app/services/config_service.py
@@ -3,8 +3,7 @@ from app.models.domain import ConfiguracaoMensagem
 
 TEMPLATE_PADRAO = (
     "Olá, {nome}! Notamos que você possui {faltas} falta(s) registrada(s). "
-    "Lembrando que esta é a última semana para realizar a reposição do mês! "
-    "Por favor, responda a esta mensagem para agendarmos o seu horário."
+    "Lembrando que esta é a última semana para realizar a reposição do mês!"
 )
 
 TEMPLATE_LEMBRETE_PADRAO = (
